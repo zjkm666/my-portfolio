8aa307bd51faa0da82089fb9dbf0750b535e6129
joshua. <jshmlnd@icloud.com>
2026-01-16 10:07:44 +0800

    Added Everything

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true,
+        }),
+        react({
+            babel: {
+                plugins: ['babel-plugin-react-compiler'],
+            },
+        }),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+            '~': fileURLToPath(new URL('./resources/src', import.meta.url)),
+        },
+    },
+    esbuild: {
+        jsx: 'automatic',
+    },
+});
